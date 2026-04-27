2205bd50dfa15fc57d604926878b3da8a1b4d8d9
Tony <140578297+tonyb-99@users.noreply.github.com>
2026-04-27 23:34:48 +0800

    Code reworked. Prev pseudocode failed to work on hardware. Now testing time independent of rfid.

--- A2-RFID-Access-Control/code/esp32-arduino/esp32-arduino.ino
+++ A2-RFID-Access-Control/code/esp32-arduino/esp32-arduino.ino
@@ -1,18 +1,43 @@
-#include <MFRC522v2.h>
-#include <MFRC522DriverSPI.h>
-#include <MFRC522DriverPinSimple.h>
-#include <MFRC522Debug.h>
+#include <MFRC522.h>
+#include <SPI.h>
+#include <Preferences.h>
 #include "file_manager.h"
 #include "sounds.h"
 
 // Code based on RandomNerdTutorial (2026). https://randomnerdtutorials.com/esp32-mfrc522-rfid-reader-arduino/
 
-#define SPEAKER_PIN 
-#define G_PIN
-#define R_PIN
+#define SPEAKER_PIN 25
+#define G_PIN   26
+#define R_PIN   27
 #define NR_KNOWN_KEYS   8
-// Known keys, see: https://code.google.com/p/mfcuk/wiki/MifareClassicDefaultKeys
-byte knownKeys[NR_KNOWN_KEYS][MFRC522::MF_KEY_SIZE] =  {
+#define TABLESIZE   50
+#define SS_PIN  5
+#define RST_PIN   21
+
+typedef enum {
+  IDLE,
+  TAPPED,
+  PROCESSING,
+  AUTHENTICATE,
+} States;
+
+States cardState = States::IDLE;
+
+Preferences prefs;
+unsigned long prevTime = 0;
+unsigned long tick = 0;
+unsigned long lastTick = -1;
+unsigned long startTick = 0;
+
+const char* passKey = "/passKey.txt";
+const bool debug = true;
+bool cardDetected = false;
+MFRC522::Uid user[TABLESIZE];
+
+MFRC522 mfrc522(SS_PIN, RST_PIN);
+MFRC522::MIFARE_Key key;
+// Known keys, see: https://code.google.com/p/mfcuk/wiki/MifareClassicfactoryKeys
+byte knownKeys[NR_KNOWN_KEYS][MFRC522::MIFARE_Misc::MF_KEY_SIZE] =  {
     {0xff, 0xff, 0xff, 0xff, 0xff, 0xff}, // FF FF FF FF FF FF = factory default
     {0xa0, 0xa1, 0xa2, 0xa3, 0xa4, 0xa5}, // A0 A1 A2 A3 A4 A5
     {0xb0, 0xb1, 0xb2, 0xb3, 0xb4, 0xb5}, // B0 B1 B2 B3 B4 B5
@@ -23,151 +48,41 @@ byte knownKeys[NR_KNOWN_KEYS][MFRC522::MF_KEY_SIZE] =  {
     {0x00, 0x00, 0x00, 0x00, 0x00, 0x00}  // 00 00 00 00 00 00
 };
 
-MFRC522DriverPinSimple ss_pin(5);
-MFRC522DiverSPI driver{ss_pin}; // Create SPI driver
-MFRC522 mfrc522{driver};  // Create MFRC522 instance
-MFRC522::MIFARE_Key key;
-
-const char* keyPath = "/keypath.txt";
-const char* userPath = "/userpath.txt";
-const bool debug = true;
-
-
 void setup()
 {
-  Serial.begin(115200);
+  Serial.begin(9600);
   while(!Serial);
-
-  initLittleFS();
+  SPI.begin();
   mfrc522.PCD_Init();
 }
 
-void loop();
-{
-  // Check card presence
-  if(!mfrc522.PICC_IsNewCardPresent() || !mfrc522.PICC_ReadCardSerial()) { delay(500); return; }
-  else { playOnTap(SPEAKER_PIN); }
-
-  Serial.print("-----------------------------------------\nCard UID:");
-  MFRC522Debug::PrintUID(Serial, (mfrc522.uid));
-  Serial.println();
-
-  // Check if new card with default key
-  for(byte j = 0; j < NR_KNOWN_KEYS; j++)
-  {
-    for(byte i = 0; i < MFRC522::MF_KEY_SIZE; i++) 
-    {
-      key.keyByte[i] = knownKeys[j][i];
+void loop() {
+    if (millis() - prevTime >= 1000) {
+        Serial.println("1 second passed");
+        prevTime = millis();
     }
 
-    if(try_key(&key))   // Authentication process ends during test.
-    {
-      Serial.println("Contains default key.");
-      break;
-    }
+    // RFID logic (independent)
+    if (!mfrc522.PICC_IsNewCardPresent())
+        return;
 
-    if(!mfrc522.PICC_IsNewCardPresent() || !mfrc522.PICC_ReadCardSerial()) { break; }
-  }
-    // Load key file (made in setup)
-    String loadedKey = readFile(LittleFS, keyPath);
-    // If key is empty, generate a key
-    if(loadedKey == "")
-    {
-      
-    }
-    // Overwrite default key with new key
-    // Register card UID to memory/file
-    // Re-authenticate
+    if (!mfrc522.PICC_ReadCardSerial())
+        return;
 
-  // Authenticate with matching key file to trailer
-  byte trailerAddress = 3;
-  if(mfrc522.PCD_Authenticate(0x60, trailerAddress, &key, &(mfrc522.uid)) != 0)     //Block address = 3
-  {
-    Serial.println("Authentication failed");
-    playDeclined(SPEAKER_PIN);
-    return;
-  }
-  
-  // Check a specific empty block on card for current status
-  // Write current status to card in an empty block
-
-
-
-
-  byte blockAddress = 2;
-  byte bufferblocksize = 18;
-  byte blockDataRead[bufferblocksize];
-  if(mfrc522.MIFARE_Read(blockAddress, blockDataRead, &bufferblocksize) != 0)
-  {
-    Serial.println("Authentication failed");
-    playDeclined(SPEAKER_PIN);
-    return;
-  }
-  else
-  {
-    Serial.println("Read successfully!");
-    playSuccess(SPEAKER_PIN);
-
-    /********************DEBUG**************************/
-    Serial.print("Data in block ");
-    Serial.print(blockAddress);
-    Serial.print(": ");
-    for (byte i = 0; i < 16; i++) {
-      Serial.print((char)blockDataRead[i]);  // Print as character
-    }
+    Serial.print(F("Card UID:"));
+    dump_byte_array(mfrc522.uid.uidByte, mfrc522.uid.size);
     Serial.println();
-    /**************************************************/
-  }
-
-  //Keep access open for short duration (3 sec)
-  //Record state to file (IN/OUT)
-  
-  // End process
-  mfrc522.PICC_HaltA();
-  mfrc522.PCD_StopCrypto1();
-
-  delay(1000);
 }
 
-
-bool try_key(MFRC522::MIFARE_Key *key)
-{
-    bool result = false;
-    byte buffer[18];
-    byte block = 0;
-    MFRC522::StatusCode status;
-
-    // Serial.println(F("Authenticating using key A..."));
-    status = mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, block, key, &(mfrc522.uid));
-    if (status != MFRC522::STATUS_OK) {
-        // Serial.print(F("PCD_Authenticate() failed: "));
-        // Serial.println(mfrc522.GetStatusCodeName(status));
-        return false;
+/**
+ * Helper routine to dump a byte array as hex values to Serial.
+ */
+void dump_byte_array(byte *buffer, byte bufferSize) {
+    for (byte i = 0; i < bufferSize; i++) {
+        Serial.print(buffer[i] < 0x10 ? " 0" : " ");
+        Serial.print(buffer[i], HEX);
     }
-
-    // Read block
-    byte byteCount = sizeof(buffer);
-    status = mfrc522.MIFARE_Read(block, buffer, &byteCount);
-    if (status != MFRC522::STATUS_OK) {
-        // Serial.print(F("MIFARE_Read() failed: "));
-        // Serial.println(mfrc522.GetStatusCodeName(status));
-    }
-    else {
-        // Successful read
-        result = true;
-        Serial.print(F("Success with key:"));
-        dump_byte_array((*key).keyByte, MFRC522::MF_KEY_SIZE);
-        Serial.println();
-        // Dump block data
-        Serial.print(F("Block ")); Serial.print(block); Serial.print(F(":"));
-        dump_byte_array(buffer, 16);
-        Serial.println();
-    }
-    Serial.println();
-
-    mfrc522.PICC_HaltA();       // Halt PICC
-    mfrc522.PCD_StopCrypto1();  // Stop encryption on PCD
-    return result;
 }
+
 
 
--- A2-RFID-Access-Control/code/esp32-arduino/file_manager.h
+++ A2-RFID-Access-Control/code/esp32-arduino/file_manager.h
@@ -7,5 +7,5 @@
 void initLittleFS();
 String readFile(fs::FS &fs, const char * path, bool debug = false);
 void writeFile(fs::FS &fs, const char * path, const char * message);
-bool deleteFile(fs::FS &fs, const char *path); 
+void deleteFile(fs::FS &fs, const char *path); 
 #endif
