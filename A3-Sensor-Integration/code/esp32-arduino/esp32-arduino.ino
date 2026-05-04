d293e7e52298b9ad614d26221e89649ba928889f
Tony <140578297+tonyb-99@users.noreply.github.com>
2026-05-04 23:06:30 +0800

    Setup new project.

---
+++ A3-Sensor-Integration/code/esp32-arduino/esp32-arduino.ino
@@ -0,0 +1,13 @@
+// https://randomnerdtutorials.com/esp32-how-to-log-data/
+// https://randomnerdtutorials.com/esp32-microsd-card-arduino/
+// https://randomnerdtutorials.com/esp32-mpu-6050-accelerometer-gyroscope-arduino/
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
---
+++ A3-Sensor-Integration/code/esp32-arduino/gyro.h
@@ -0,0 +1,13 @@
+// #ifndef GYRO_H
+// #define GYRO_H
+
+// #include <Adafruit_MPU6050.h>
+// #include <Adafruit_Sensor.h>
+// #include <Arduino.h>
+// #include <Wire.h>
+
+// Adafruit_MPU6050 mpu;
+
+// void initGyro();
+
+// #endif
